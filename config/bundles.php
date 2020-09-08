cfc6e618f3c73e49857f58464a6a7cb761be2614
Shiiyo <camaille13@hotmail.fr>
2020-09-08 10:22:15 +0200

    Solve probleme of migrations for the Readme

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
